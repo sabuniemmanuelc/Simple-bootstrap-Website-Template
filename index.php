d22387d9c3b5148b82fc478fec2359401a700443
Sabuni Emmanuel C <sabuni.emmanuelc@gmail.com>
2020-04-13 12:46:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Sabuni Emmanuel C">
+    <meta name="description" content="Professional Website Template">
+    <meta name="keywords" content="web template, professional website, web design">
+    <title>Developers at Zed | Web solutions</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/fixed.css">
+  </head>
+  <body>
+  <!-- Header -->
+    <header id="header" class="skel-layers-fixed">
+      <h1><a href="#">Developers at Zed</a></h1>
+      <nav id="nav">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="about.php">About Us</a></li>
+          <li><a href="service.php">Services</a></li>
+          <li><a href="blog">Blog</a></li>
+          <li><a href="contact.php">Contact Us</a></li>
+        </ul>
+      </nav>
+    </header>
+    <!-- Banner background -->
+      <section id="banner">
+        <div class="inner">
+          <h2>Professional IT Solutions</h2>
+          <p>Responsive Website | Mobile Apps | Security Solutions</p>
+          <ul class="actions">
+            <li><a href="#content" class="button big special">Get Started</a></li>
+          </ul>
+        </div>
+      </section>
+      <section id="newslettersec">
+      <h2 class="text-center"> Subscribe To Our Newsletter</h2>
+      <form>
+        <input type="email" class="inputemail" placeholder="Enter Email.....">
+        <button type="submit" class="button big special">Subscribe</button>
+      </form>
+  </section>
+      <!-- Footer -->
+  			<footer id="footer">
+  				<div class="container">
+  					<ul class="copyright">
+  						<li>&copy; <?=date("Y");?>. All rights reserved.</li>
+  						<li>Developers at Zed</li>
+  						<li>Images: <a href="http://unsplash.com">Unsplash</a></li>
+  					</ul>
+  				</div>
+  			</footer>
+    <!-- scripts --->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- end scripts -->
+  </body>
+</html>
